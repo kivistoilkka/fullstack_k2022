a3da07f1ec5c9596723f05ec039ae6f3082b8b70
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-01-31 17:45:53 +0200

    Exercise 1.11: unicafe step6

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -7,9 +7,14 @@ const Button = ({ handleClick, text }) => (
 )
 
 const StatisticLine = ({ text, value }) => (
-  <div>
-    {text} {value}
-  </div>
+  <tr>
+    <td>
+      {text}
+    </td>
+    <td>
+    {value}
+    </td>
+  </tr>
 )
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -23,14 +28,16 @@ const Statistics = ({ good, neutral, bad }) => {
   const calcAverage = () => (good*1+bad*(-1)) / (good+neutral+bad)
   const calcPositivePercent = () => good/(good+neutral+bad)*100
   return (
-    <div>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={good+neutral+bad} />
-      <StatisticLine text='average' value={calcAverage()} />
-      <StatisticLine text='positive' value={calcPositivePercent() + ' %'} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={good+neutral+bad} />
+        <StatisticLine text='average' value={calcAverage()} />
+        <StatisticLine text='positive' value={calcPositivePercent().toString().concat(' %')} />
+      </tbody>
+    </table>
   )
 }
 
