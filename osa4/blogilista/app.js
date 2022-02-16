eecda319a4ab719ee9585a8930026da1343b988a
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-02-16 18:04:14 +0200

    Exercise 4.2: blogilista, step2: Middlewares, error catching and tidying up

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,19 +1,31 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
-
-const logger = require('./utils/logger')
-const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
 
 const mongoUrl = config.MONGODB_URI
+logger.info('connecting to', mongoUrl)
 mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB', error.message)
+  })
 
 app.use(cors())
 app.use(express.json())
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
