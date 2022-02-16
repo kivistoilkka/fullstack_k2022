84d3894f06b93c6850d269e38e8516482c1444d0
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-02-16 17:18:41 +0200

    Exercise 4.2: blogilista, step2: Controller module

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,4 +1,3 @@
-//const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -7,15 +6,7 @@ require('dotenv').config()
 
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const blogsRouter = require('./controllers/blogs')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
@@ -23,22 +14,6 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
