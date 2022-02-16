986b17f1b9105ac03c7eb8c70c41e6a85b4f57e2
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-02-16 21:17:21 +0200

    Exercise 4.5: apufunktioita ja yksikkötestejä, step3

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,5 +1,5 @@
 const dummy = (blogs) => {
-  blogs // to keep ESLint quiet
+  blogs // to keep ESLint happy
   return 1
 }
 
@@ -10,7 +10,30 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const firstBlog = blogs[0]
+
+  if (firstBlog === undefined) {
+    return undefined
+  }
+
+  const formatBlog = (blog) => {
+    return {
+      title: blog.title,
+      author: blog.author,
+      likes: blog.likes
+    }
+  }
+  const moreLikes = (most, blog) => {
+    return most.likes < formatBlog(blog).likes
+      ? formatBlog(blog)
+      : most
+  }
+  return blogs.reduce(moreLikes, formatBlog(firstBlog))
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
