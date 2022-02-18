2a54b6d9bda9669e2fca2826fd6c51efe617e744
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-02-18 20:21:40 +0200

    Exercise 4.10: blogilistan testit step3, test

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -32,10 +32,30 @@ test('a specific blog is within the returned blogs', async () => {
 test('id property is present and _id property is not present', async () => {
   const response = await api.get('/api/blogs')
   response.body.map(b => {
-    console.log(b)
     expect(b.id).toBeDefined()
     expect(b._id).not.toBeDefined()
   })
+})
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Super interesting blog',
+    author: 'Imaginary Person',
+    url: 'http://made.up.address/',
+    likes: 9
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  const titles = response.body.map(r => r.title)
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(titles).toContain('Super interesting blog')
 })
 
 afterAll(() => {
