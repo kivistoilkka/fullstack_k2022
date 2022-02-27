2959f2b0c49b65edc7c8387c53a23be7f69708a9
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-02-28 00:24:09 +0200

    Exercises 5.13 and 5.14: blogilistan testit, step1 and step2

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -24,28 +25,43 @@ describe('<Blog />', () => {
     blogs: []
   }
 
-  const mockHandler = jest.fn()
+  const increaseMockHandler = jest.fn()
+  const deleteMockHandler = jest.fn()
 
   beforeEach(() => {
     container = render(
       <Blog
         blog={blog}
-        increaseLikes={mockHandler}
-        deleteBlog={mockHandler}
+        increaseLikes={increaseMockHandler}
+        deleteBlog={deleteMockHandler}
         user={user}
       />
     ).container
   })
 
   test('renders blog title and author, but not url and likes by default', () => {
-    const blogDiv = container.querySelector('.blog')
-    expect(blogDiv).toHaveTextContent('TestBlog TestAuthor')
-
     const visibleDiv = container.querySelector('.visibleByDefaultBlogInfo')
+    expect(visibleDiv).toHaveTextContent('TestBlog TestAuthor', { exact: false })
+    expect(visibleDiv).not.toHaveTextContent('http://www.helsinki.fi', { exact: false })
+    expect(visibleDiv).not.toHaveTextContent('likes 0', { exact: false })
     expect(visibleDiv).not.toHaveStyle('display: none')
 
     const hiddenDiv = container.querySelector('.hiddenByDefaultBlogInfo')
+    expect(hiddenDiv).toHaveTextContent('TestBlog TestAuthor', { exact: false })
+    expect(hiddenDiv).toHaveTextContent('http://www.helsinki.fi', { exact: false })
+    expect(hiddenDiv).toHaveTextContent('likes 0', { exact: false })
     expect(hiddenDiv).toHaveStyle('display: none')
+  })
+
+  test('url and likes are also visible after the view button is pressed', () => {
+    const button = screen.getByText('view')
+    userEvent.click(button)
+
+    const visibleDiv = container.querySelector('.visibleByDefaultBlogInfo')
+    const hiddenDiv = container.querySelector('.hiddenByDefaultBlogInfo')
+
+    expect(visibleDiv).toHaveStyle('display: none')
+    expect(hiddenDiv).not.toHaveStyle('display: none')
   })
 })
 
