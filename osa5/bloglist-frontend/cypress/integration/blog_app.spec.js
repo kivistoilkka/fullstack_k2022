a5a5368ad18af794f97ee1ea382197164ef95177
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-02-28 23:02:16 +0200

    Exercise 5.18: blogilistan end to end -testit, step2

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Test Person',
+      username: 'tester',
+      password: 'sekret'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +15,27 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('tester')
+      cy.get('#password').type('sekret')
+      cy.get('#login-button').click()
+
+      cy.contains('Test Person logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('tester')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.notification')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color','rgb(255, 0, 0)')
+
+      cy.get('html').should('not.contain', 'Test Person logged in')
+    })
   })
 })
