ebf5a3285bebdf332ee8be3e5ee7c85d00cf3b89
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-03-14 00:58:02 +0200

    Exercise 7.18: kommentit, step1, api created for posting comments

--- osa7/bloglist-backend/controllers/blogs.js
+++ osa7/bloglist-backend/controllers/blogs.js
@@ -4,9 +4,7 @@ const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog
-    .find({})
-    .populate('user', { username: 1, name: 1 })
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -23,7 +21,8 @@ blogsRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
-    user: user._id
+    user: user._id,
+    comments: [],
   })
 
   const savedBlog = await blog.save()
@@ -36,34 +35,58 @@ blogsRouter.post('/', async (request, response) => {
 blogsRouter.delete('/:id', async (request, response) => {
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
   const blog = await Blog.findById(request.params.id)
-  if (!request.token
-    || !decodedToken.id
-    || !blog
-    || blog.user.toString() !== decodedToken.id.toString()) {
-    return response.status(401).json({ error: 'token missing or invalid or id invalid' })
+  if (
+    !request.token ||
+    !decodedToken.id ||
+    !blog ||
+    blog.user.toString() !== decodedToken.id.toString()
+  ) {
+    return response
+      .status(401)
+      .json({ error: 'token missing or invalid or id invalid' })
   }
 
   const user = await User.findById(decodedToken.id)
   const removedBlog = await Blog.findByIdAndRemove(request.params.id)
-  user.blogs = user.blogs.filter(b => b._id.toString() !== removedBlog._id.toString())
+  user.blogs = user.blogs.filter(
+    (b) => b._id.toString() !== removedBlog._id.toString()
+  )
   await user.save()
   response.status(204).end()
 })
 
 blogsRouter.put('/:id', async (request, response) => {
-  const { title, author, url, likes } = request.body
+  const { title, author, url, likes, comments } = request.body
 
   const updatedBlog = await Blog.findByIdAndUpdate(
     request.params.id,
-    { title, author, url, likes },
-    { new: true, runValidators: true, context: 'query' })
+    { title, author, url, likes, comments },
+    { new: true, runValidators: true, context: 'query' }
+  )
 
   if (updatedBlog) {
     response.json(updatedBlog)
   } else {
     response.status(404).end()
   }
+})
 
+blogsRouter.post('/:id/comments', async (request, response) => {
+  const comment = request.body.comment
+  const blogToComment = await Blog.findById(request.params.id)
+  if (!blogToComment) {
+    response.status(404).end()
+  }
+
+  const updatedComments = blogToComment.comments.concat(comment)
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { comments: updatedComments },
+    { new: true, runValidators: true, context: 'query' }
+  )
+
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- osa7/bloglist-backend/models/blog.js
+++ osa7/bloglist-backend/models/blog.js
@@ -3,18 +3,19 @@ const mongoose = require('mongoose')
 const blogSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   author: String,
   url: {
     type: String,
-    required: true
+    required: true,
   },
   likes: Number,
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }
+    ref: 'User',
+  },
+  comments: Array,
 })
 
 blogSchema.set('toJSON', {
@@ -22,7 +23,7 @@ blogSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  }
+  },
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
