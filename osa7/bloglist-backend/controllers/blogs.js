1e14d0fd3efb0240c10d1c07c6081ce9193d09ef
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-03-14 01:39:22 +0200

    Exercise 7.18: kommentit, step1, id added for each comment

--- osa7/bloglist-backend/controllers/blogs.js
+++ osa7/bloglist-backend/controllers/blogs.js
@@ -3,6 +3,8 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
+const getId = () => (100000 * Math.random()).toFixed(0)
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
@@ -73,12 +75,19 @@ blogsRouter.put('/:id', async (request, response) => {
 
 blogsRouter.post('/:id/comments', async (request, response) => {
   const comment = request.body.comment
+  const commentId = getId()
+
   const blogToComment = await Blog.findById(request.params.id)
   if (!blogToComment) {
     response.status(404).end()
   }
+  console.log(comment)
 
-  const updatedComments = blogToComment.comments.concat(comment)
+  const updatedComments = blogToComment.comments.concat({
+    id: commentId,
+    comment,
+  })
+  console.log(updatedComments)
 
   const updatedBlog = await Blog.findByIdAndUpdate(
     request.params.id,
