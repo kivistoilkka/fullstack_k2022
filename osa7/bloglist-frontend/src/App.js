25a2755ef09dbd6c316275aba42494a1b9a91b86
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-03-13 02:32:37 +0200

    Exercise 7.13: redux, step4, refactored login form to a component and module

--- osa7/bloglist-frontend/src/App.js
+++ osa7/bloglist-frontend/src/App.js
@@ -1,18 +1,16 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import Blog from './components/Blog'
 import Notification from './components/Notification'
 import Togglable from './components/Togglable'
 import BlogForm from './components/BlogForm'
+import LoginForm from './components/LoginForm'
 
 import { createNotification } from './reducers/notificationReducer'
 import { createBlog, initializeBlogs } from './reducers/blogReducer'
-import { loginFromLocalStorage, login, logout } from './reducers/userReducer'
+import { loginFromLocalStorage, logout } from './reducers/userReducer'
 
 const App = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
   const dispatch = useDispatch()
   const blogs = useSelector(({ blogs }) => blogs)
   const user = useSelector(({ user }) => user)
@@ -31,46 +29,9 @@ const App = () => {
     }
   }, [])
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    dispatch(login(username, password))
-    setUsername('')
-    setPassword('')
-  }
-
   const handleLogout = () => {
     dispatch(logout())
   }
-
-  const loginForm = () => (
-    <div>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            id="username"
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            id="password"
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button id="login-button" type="submit">
-          login
-        </button>
-      </form>
-    </div>
-  )
 
   const addBlog = async (blogObject) => {
     const { title, author, url } = blogObject
@@ -96,7 +57,7 @@ const App = () => {
       <div>
         <h2>Log in to application</h2>
         <Notification />
-        {loginForm()}
+        <LoginForm />
       </div>
     )
   }
---
+++ osa7/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { login } from '../reducers/userReducer'
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const dispatch = useDispatch()
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    dispatch(login(username, password))
+    setUsername('')
+    setPassword('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            id="username"
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            id="password"
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button id="login-button" type="submit">
+          login
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
