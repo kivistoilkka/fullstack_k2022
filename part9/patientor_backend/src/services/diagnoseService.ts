a7dec65310b02ef1d68c8b776ebf2b5d12530e8b
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-07-06 16:33:59 +0300

    Exercise 9.10: Patientor backend, step3

---
+++ part9/patientor_backend/src/services/diagnoseService.ts
@@ -0,0 +1,13 @@
+import diagnoseData from '../../data/diagnoses.json';
+
+import { Diagnose } from '../types';
+
+const diagnoses: Array<Diagnose> = diagnoseData;
+
+const getDiagnoses = () => {
+  return diagnoses;
+};
+
+export default {
+  getDiagnoses
+};
