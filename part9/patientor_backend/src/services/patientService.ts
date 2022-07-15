e8d7efe47140554f91a8a2d22aae5150fced4225
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-07-15 16:16:47 +0300

    Exercise 9.12: Patientor backend, step5

--- part9/patientor_backend/src/services/patientService.ts
+++ part9/patientor_backend/src/services/patientService.ts
@@ -1,6 +1,8 @@
+import { v1 as uuid } from 'uuid';
+
 import patientData from '../../data/patients.json';
 
-import { Patient, PatientWithoutSSN } from '../types';
+import { Patient, PatientWithoutSSN, NewPatient } from '../types';
 
 const patients: Array<Patient> = patientData;
 
@@ -14,6 +16,17 @@ const getPatients = (): PatientWithoutSSN[] => {
   }));
 };
 
+const addPatient = (entry: NewPatient): Patient => {
+  const newPatientEntry = {
+    id: uuid(),
+    ...entry
+  };
+
+  patients.push(newPatientEntry);
+  return newPatientEntry;
+};
+
 export default {
-  getPatients
+  getPatients,
+  addPatient
 };
