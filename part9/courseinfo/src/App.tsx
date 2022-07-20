7acc5acb1ed28b73f132e8f8202a16e7f64f960e
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2022-07-20 20:19:45 +0300

    Exercise 9.15, new interface with description attribute

--- part9/courseinfo/src/App.tsx
+++ part9/courseinfo/src/App.tsx
@@ -1,10 +1,35 @@
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CoursePartDescription {
+  type: "normal";
+}
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartDescription {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
 const Header = ({ name }: { name: string }) => {
   return <h1>{name}</h1>;
 };
 
 interface CourseProps {
   parts: { name: string, exerciseCount: number }[];
-};
+}
 
 const Content = ({ parts }: CourseProps) => {
   return (
@@ -31,21 +56,33 @@ const Total = ({ parts }: CourseProps) => {
   );
 };
 
-
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
     }
   ];
 
